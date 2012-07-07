c1740b168167c98ae3fa2c02da1da01b737e8c98
Evan Purcer <evanpurcer@gmail.com>
2012-07-06 18:08:28 -0700

    Add views for main canvas and control panel

---
+++ js/views/canvas.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+
+    'text!templates/canvas.html'
+
+], function ($, _, Backbone, template) {
+
+    return Backbone.View.extend({
+
+        el: '#canvas',
+
+        render: function () {
+
+        }
+    });
+});
---
+++ js/views/controls.js
@@ -0,0 +1,20 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'jqhotkeys',
+    'jqlongkeys',
+
+    'text!templates/controls.html'
+
+], function ($, _, Backbone, hotkeys, longkeys, template) {
+
+    return Backbone.View.extend({
+
+        el: '#controls',
+
+        render: function () {
+
+        }
+    });
+});
