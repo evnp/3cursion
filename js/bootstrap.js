1dd52537d1781a8b72cc27a8458b1f0a81fd8d73
Evan Purcer <evanpurcer@gmail.com>
2012-07-09 16:01:02 -0700

    INTERMEDIATE COMMIT

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,6 +5,7 @@ require.config({
         jqlongkeys: 'libs/jquery/plugins/jquery.longkeys.min',
         underscore: 'libs/underscore/underscore-1.3.2-amd-min',
         backbone:   'libs/backbone/backbone-0.9.2-amd-min',
+        three:      'libs/three/three.min',
 
         text: 'libs/require/text',
         templates: '../templates'
--- js/views/canvas.js
+++ js/views/canvas.js
@@ -2,17 +2,36 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'three'
 
-    'text!templates/canvas.html'
-
-], function ($, _, Backbone, template) {
+], function ($, _, Backbone, Three) {
 
     return Backbone.View.extend({
 
         el: '#canvas',
 
         render: function () {
+            var w = window.innerWidth
+              , h = window.innerHeight
+              , materialProperties = { color: 0xff0000, wireframe: true }
 
+              , scene    = new THREE.Scene()
+              , camera   = new THREE.PerspectiveCamera(75, w/h, 1, 10000)
+              , geometry = new THREE.CubeGeometry(200,200,200)
+              , material = new THREE.MeshBasicMaterial(materialProperties)
+              , mesh     = new THREE.Mesh(geometry, material)
+              , renderer
+              ;
+
+            camera.position.z = 1000;
+
+            scene.add(camera);
+            scene.add(mesh);
+
+            renderer = new THREE.CanvasRenderer();
+            renderer.setSize(w, h);
+
+            this.$el.append(renderer.domElement);
         }
     });
 });
