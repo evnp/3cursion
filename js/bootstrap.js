adac4d329b5f6aec76312d2061767febf8e31fc3
Evan Purcer <evanpurcer@gmail.com>
2012-07-11 12:58:45 -0700

    Add webGL detector

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -6,6 +6,7 @@ require.config({
         underscore: 'libs/underscore/underscore-1.3.2-amd-min',
         backbone:   'libs/backbone/backbone-0.9.2-amd-min',
         three:      'libs/three/three.min',
+        detector:   'libs/three/Detector',
 
         text: 'libs/require/text',
         templates: '../templates'
