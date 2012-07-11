514970df5028b29fe2d1b46953f05be5f3d476c9
Evan Purcer <evanpurcer@gmail.com>
2012-07-11 12:59:51 -0700

    Add lighting and shaded wireframe rendering

--- js/views/canvas.js
+++ js/views/canvas.js
@@ -2,54 +2,114 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'three'
+    'three',
+    'detector',
 
-], function ($, _, Backbone, Three) {
+], function ($, _, Backbone) {
+
+    // Constants
+    var RAD = 1600
+      , PI  = Math.PI
+      , THT = 45 // Theta
+      , PHI = 60
+      , RENDERER = (Detector.webgl ? 'WebGL' : 'Canvas') + 'Renderer'
+      ;
 
     return Backbone.View.extend({
 
         el: '#canvas',
 
+        initialize: function () {
+            this.w = this.$el.width();
+            this.h = this.$el.height() - 4;
+
+        // Camera
+            this.camera = new THREE.PerspectiveCamera(75, this.w/this.h, 1, 10000);
+            /*
+            this.camera.position.x = RAD * Math.sin(THT*PI/360)
+                                               * Math.cos(PHI*PI/360);
+            this.camera.position.y = RAD * Math.sin(PHI*PI/360);
+            this.camera.position.z = RAD * Math.cos(THT*PI/360)
+                                               * Math.cos(PHI*PI/360);
+            */
+            this.camera.position.z = 1000;
+
+        // Mesh
+            this.getShadedMesh = function (geometry) {
+                return new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
+                    color: 0xffffff
+                }));
+            };
+
+            this.getWireframeMesh = function (geometry) {
+                return new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+                    color: 0xff0000,
+                    wireframe: true
+                }));
+            };
+
+            this.getWireframeShadedMesh = function (geometry) {
+                return THREE.SceneUtils.createMultiMaterialObject(geometry, [
+                    new THREE.MeshLambertMaterial({
+                        color: 0xffffff
+                    }),
+                    new THREE.MeshBasicMaterial({
+                        color: 0x000000,
+                        wireframe: true
+                    })
+                ]);
+            };
+
+            this.mesh = this.getWireframeShadedMesh(
+                new THREE.CubeGeometry(200, 200, 200)
+            );
+
+        // Scene
+            this.scene = new THREE.Scene();
+            this.scene.add(this.camera);
+            this.scene.add(this.mesh);
+
+        // Lights
+            var light1 = new THREE.DirectionalLight(0xffffff);
+            light1.position.x = 1;
+            light1.position.y = 1;
+            light1.position.z = 0.75;
+            light1.position.normalize();
+
+            var light2 = new THREE.DirectionalLight(0x808080);
+            light2.position.x = -1;
+            light2.position.y = 1;
+            light2.position.z = -0.75;
+            light2.position.normalize();
+
+            this.scene.add(new THREE.AmbientLight(0x404040));
+            this.scene.add(light1);
+            this.scene.add(light2);
+
+        // Renderer
+            this.renderer = new THREE[RENDERER]();
+            this.renderer.setSize(this.w, this.h);
+        },
+
         render: function () {
-            var w = this.$el.width()
-              , h = this.$el.height() - 4
-              , materialProperties = { color: 0xff0000, wireframe: true }
+            var canvas = this;
 
-              , scene    = new THREE.Scene()
-              , camera   = new THREE.PerspectiveCamera(75, w/h, 1, 10000)
-              , geometry = new THREE.CubeGeometry(200,200,200)
-              , material = new THREE.MeshBasicMaterial(materialProperties)
-              , mesh     = new THREE.Mesh(geometry, material)
-              , renderer
-              ;
-
-            camera.position.z = 1000;
-
-            scene.add(camera);
-            scene.add(mesh);
-
-            renderer = new THREE.CanvasRenderer();
-            renderer.setSize(w, h);
-
-            this.$el.append(renderer.domElement);
+            canvas.$el.append(canvas.renderer.domElement);
 
             animate();
 
             function animate() {
-
-                // note: three.js includes requestAnimationFrame shim
-                requestAnimationFrame( animate );
+                requestAnimationFrame(animate);
                 render();
-
             }
 
             function render() {
-
-                mesh.rotation.x += 0.01;
-                mesh.rotation.y += 0.02;
-
-                renderer.render( scene, camera );
-
+                canvas.mesh.rotation.x += 0.01;
+                canvas.mesh.rotation.y += 0.02;
+                canvas.renderer.render(
+                    canvas.scene,
+                    canvas.camera
+                );
             }
         }
     });
