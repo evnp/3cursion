39ec5957ee6dd4b2c78bddf2517460bd59b77eb0
Evan Purcer <evanpurcer@gmail.com>
2012-07-12 20:14:32 -0700

    INTERMEDIATE COMMIT

--- js/views/canvas.js
+++ js/views/canvas.js
@@ -13,8 +13,10 @@ define([
       , THT = 45 // Theta
       , PHI = 60
       , FOV = 70 // Field of view for perspective camera
+      , BLACK = 0x000000
+      , RED   = 0xff0000
       , RENDERER = (Detector.webgl ? 'WebGL' : 'Canvas') + 'Renderer'
-      , MOUSESPEED = 0.4 // Speed of mouse camera rotation
+      , CAMSPEED = 0.4 // Speed of mouse camera rotation
       ;
 
     return Backbone.View.extend({
@@ -35,7 +37,7 @@ define([
 
             this.projector = new THREE.Projector();
 
-        // Mesh
+        // Cubes
             this.getShadedMesh = function (geometry) {
                 return new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                     color: 0xffffff
@@ -61,14 +63,35 @@ define([
                 ]);
             };
 
-            this.mesh = this.getWireframeShadedMesh(
+            var cube1 = this.getWireframeShadedMesh(
                 new THREE.CubeGeometry(20, 20, 20)
             );
+
+            var cube2 = this.getWireframeShadedMesh(
+                new THREE.CubeGeometry(20, 20, 20)
+            );
+            cube2.position.x = 40;
+
+        // Reference Plane
+            this.plane = new THREE.Mesh(
+                new THREE.PlaneGeometry( 2000, 2000, 8, 8 ),
+                new THREE.MeshBasicMaterial({
+                    color: 0x000000, opacity: 0.25,
+                    transparent: true,
+                    wireframe: true
+                })
+            );
+            this.plane.lookAt(this.camera.position);
+            this.plane.visible = false;
+
+            this.planeOffset = new THREE.Vector3();
 
         // Scene
             this.scene = new THREE.Scene();
             this.scene.add(this.camera);
-            this.scene.add(this.mesh);
+            this.scene.add(cube1);
+            this.scene.add(cube2);
+            this.scene.add(this.plane);
 
         // Lights
             var light1 = new THREE.DirectionalLight(0xffffff);
@@ -90,7 +113,7 @@ define([
         // Renderer
             this.renderer = new THREE[RENDERER]();
 
-        // Events
+        // Misc.
             var canvas = this;
             window.addEventListener( 'resize', onWindowResized, false );
 
@@ -101,7 +124,9 @@ define([
 
             onWindowResized(null);
 
-            this.setUpObjectSelection();
+        // Setup Interface
+            this.setupObjHover();
+            this.setupObjMovement();
         },
 
         render: function () {
@@ -136,8 +161,8 @@ define([
                     mouseY = e.clientY; mouseLat = canvas.lat;
 
                     canvas.$el.on('mousemove.cam', function (e) {
-                        canvas.lon = ( e.clientX - mouseX ) * MOUSESPEED + mouseLon;
-                        canvas.lat = ( e.clientY - mouseY ) * MOUSESPEED + mouseLat;
+                        canvas.lon = ( e.clientX - mouseX ) * CAMSPEED + mouseLon;
+                        canvas.lat = ( e.clientY - mouseY ) * CAMSPEED + mouseLat;
                     });
 
                     canvas.$el.on('mouseup.cam', function (e) {
@@ -153,13 +178,22 @@ define([
             this.$el.contextmenu(function (e) { return false; });
 
         // Camera Zoom
-            this.$el.get(0).addEventListener( 'mousewheel',     onMouseWheel, false );
-            this.$el.get(0).addEventListener( 'DOMMouseScroll', onMouseWheel, false );
+            this.el.addEventListener( 'mousewheel',     onMouseWheel, false );
+            this.el.addEventListener( 'DOMMouseScroll', onMouseWheel, false );
             function onMouseWheel( event ) {
-                if ( event.wheelDeltaY ) FOV -= event.wheelDeltaY * 0.05; // Webkit
-                else if ( event.wheelDelta ) FOV -= event.wheelDelta * 0.05; // Opera / Explorer 9
-                else if ( event.detail ) FOV += event.detail * 1.0; // Firefox
-                canvas.camera.projectionMatrix.makePerspective( FOV, W/H, 1, 1100 );
+
+                if ( event.wheelDeltaY )
+                    FOV -= event.wheelDeltaY * 0.05; // Webkit
+
+                else if (  event.wheelDelta )
+                    FOV -= event.wheelDelta * 0.05; // Opera / Explorer 9
+
+                else if (  event.detail )
+                    FOV += event.detail * 1.0; // Firefox
+
+                canvas.camera.projectionMatrix.makePerspective(
+                    FOV, W/H, 1, 1100
+                );
             }
         },
 
@@ -176,58 +210,120 @@ define([
             this.camera.lookAt(this.camera.target);
         },
 
-        setUpObjectSelection: function () {
+        setupObjHover: function () {
             var canvas = this;
 
             canvas.$el.mousemove(function (e) {
-                var hovered = canvas.getHoveredObject(e.clientX, e.clientY);
+                var hovered = canvas.getHoveredAt(e.clientX, e.clientY);
 
-                // If the hovered object has changed, set colors
+                // If the hovered object has changed,
+                // set colors, then set new hovered
                 if (hovered != canvas.hovered) {
-                    if (canvas.hovered)
-                          canvas.hovered.material.color.setHex(0x000000);
-                    if (hovered) hovered.material.color.setHex(0xff0000);                   
+                    canvas.setColor(canvas.hovered, BLACK);
                     canvas.hovered = hovered;
+                    canvas.setColor(canvas.hovered, RED);
                 }
             });
+        },
+
+        setupObjMovement: function () {
+            var canvas = this;
 
             canvas.$el.mousedown(function (e) {
                 if (e.which === 1 && canvas.hovered) {
-                    canvas.$el.on('mousemove.sel', function (e) {
+
+                    // Set plane offset for new mouseDown start position
+                    var intersect = canvas.getIntersectBetween(
+                            e.clientX, e.clientY, canvas.plane
+                        );
+                    if (intersect) {
+                        canvas.planeOffset.copy(
+                            intersect.point
+                        ).subSelf(canvas.plane.position);
+                    }
+
+                    canvas.el.style.cursor = 'move';
+
+                    canvas.$el.on('mousemove.hov', function (e) {
+                        if (canvas.hovered) {
+                            var intersect = canvas.getIntersectBetween(
+                                     e.clientX, e.clientY, canvas.plane)
+
+                            if (intersect) {
+                                var newPosition = intersect.point.subSelf(canvas.planeOffset)
+                                canvas.moveHoveredTo(newPosition);
+                            }
+                        }
                     });
 
-                    canvas.$el.on('mouseup.sel', function (e) {
+                    canvas.$el.on('mouseup.hov', function (e) {
                         if (e.which === 1) {
-                            canvas.$el.off('mousemove.sel');
-                            canvas.$el.off('mouseup.sel');
+                            canvas.$el.off('mousemove.hov');
+                            canvas.$el.off('mouseup.hov');
+                            canvas.el.style.cursor = 'auto';
                         }
                     });
                 }
             });
         },
 
-    // Utility
-        getWireframeObjects: function () {
-            return _.filter(this.scene.__objects, function (obj) {
-                return obj.material && obj.material.wireframe;
-            });
+        moveHoveredTo: function(position) {
+            if (this.hovered && this.hovered.wireframe && this.hovered.shaded) {
+                this.hovered.wireframe.position.copy(position);
+                this.hovered.shaded.position.copy(position);
+            }
         },
 
-        getHoveredObject: function (x, y) {
+    // Utility
+        getAllObjects: function () {
+            var objects = _.filter(this.scene.__objects, function (obj) {
+                    return obj.geometry && obj.geometry instanceof THREE.CubeGeometry;
+                })
+              , wireframes = _.filter(objects, function (obj) {
+                    return obj.material.wireframe;
+                });
+
+            return {
+                wireframe: wireframes,
+                shaded: _.difference(objects, wireframes)
+            };
+        },
+
+        getHoveredAt: function (x, y) {
+            var objects   = this.getAllObjects()
+              , intersect = this.getIntersectBetween(x, y, objects.wireframe)
+              , wireframe = intersect ? intersect.object : null
+              , index     = objects.wireframe.indexOf(wireframe);
+
+            return {
+                wireframe: objects.wireframe[index] || null,
+                shaded:    objects.shaded[index]    || null
+            };
+        },
+
+        getIntersectBetween: function (x, y, obj) {
+            var ray = this.getRayAt(x, y)
+
+            return ( obj instanceof Array  ?
+                 ray.intersectObjects(obj) :
+                  ray.intersectObject(obj) )[0] || null;
+        },
+
+        getRayAt: function (x, y) {
+
             var vector = new THREE.Vector3(
                 ((x/ W) * 2) - 1,
-               -((y/ H) * 2) + 1,
-                0.5
-            );
+               -((y/ H) * 2) + 1, 0.5);
 
             this.projector.unprojectVector(vector, this.camera);
 
-            var ray = new THREE.Ray(this.camera.position,
-                     vector.subSelf(this.camera.position).normalize())
+            return new THREE.Ray(this.camera.position,
+                  vector.subSelf(this.camera.position).normalize());
+        },
 
-              , intersects = ray.intersectObjects(this.getWireframeObjects())
-
-            return intersects.length > 0 ? intersects[0].object : null;
+        setColor: function (obj, color) {
+            if (obj && obj.wireframe)
+                obj.wireframe.material.color.setHex(color);
         }
     });
 });
