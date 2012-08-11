8771c3d35379e73474e8887747794490056067f0
Evan Purcer <evanpurcer@gmail.com>
2012-08-11 12:46:30 -0700

    Add returns for method chaining

--- js/collections/cubes.js
+++ js/collections/cubes.js
@@ -11,15 +11,6 @@ define([
 
         model: Cube,
 
-        /*initialize: function (models, scene) {
-            // Automatically add cube instances to scene
-            if (scene) {
-                this.on('add', function (cube) {
-                    scene.add(cube.get('object'));
-                });
-            }
-        },*/
-
         wireframes: function () {
             return this.pluck('wireframe');
         },
@@ -36,37 +27,25 @@ define([
 
         moveAll: function (movement) {
             this.each( function (cube) { cube.move(movement); });
-        },
-
-        updatePositions: function () {
-            this.each( function (cube) { cube.updatePosition(); });
+            return this;
         },
 
         rotateAll: function (movement, mouseX, mouseY) {
             this.each( function (cube) {
                 cube.rotate(movement, mouseX, mouseY);
             });
-        },
-
-        updateRotations: function () {
-            this.each( function (cube) { cube.updateRotation(); });
+            return this;
         },
 
         scaleAll: function (factor) {
             this.each( function (cube) { cube.scale(factor); });
-        },
-
-        recurseAll: function () {
-            // Collect added children from each cube's recursion and
-            // return them all, so that they can be added to the scene.
-            return _.flatten( this.map( function (cube) {
-                return cube.recurse();
-            }));
+            return this;
         },
 
         deselectAll: function () {
             this.each( function (cube) { cube.select(false); });
             this.reset();
+            return this;
         }
     });
 
