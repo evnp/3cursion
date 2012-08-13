c254044716b425e5c9aa9f8dd01d8478930e6771
Evan Purcer <evanpurcer@gmail.com>
2012-08-13 14:04:02 -0700

    Make controls collapsible

--- js/views/controls.js
+++ js/views/controls.js
@@ -5,15 +5,17 @@ define([
     'jqhotkeys',
     'jqlongkeys',
 
-    'text!templates/controls.html'
+    'text!templates/controls.html',
 
-], function ($, _, Backbone, hotkeys, longkeys, template) {
+    'views/demo'
+
+], function ($, _, Backbone, hotkeys, longkeys, template, Demo) {
 
     return Backbone.View.extend({
 
         el: '#controls',
 
-        render: function () {
+        render: function (canvas) {
 
             var controls = navigator.appVersion.indexOf("Mac") != -1 ?
               [ 'double click - create cube'
@@ -43,7 +45,56 @@ define([
                 params.entries.push(control[1]);
             });
 
+            // Render controls
             this.$el.html(_.template(template)(params));
+
+            // Set up show/hide controls arrow
+            var controls = $('#controls')
+              , arrow = controls.find('div.show-hide');
+            arrow.click(function (e) {
+                var newAngle = parseFloat(arrow.css('text-indent')) ? 0 : -60
+                  , text   = controls.find('div.text')
+                  , noFade = controls.find('div.no-fade');
+
+                // Arrow Rotation:
+                // jQuery can't animate transforms. So we animate a property
+                // we don't care about (textIndent) and feed the value to a
+                // step function that will set the transform value properly.
+                arrow.animate({ textIndent: newAngle }, {
+                    step: function(angle) {
+                        var value = 'rotate(' + angle + 'deg)';
+                        arrow.css('-webkit-transform', value);
+                        arrow.css('-moz-transform',    value);
+                        arrow.css('transform',         value);
+                    }
+                });
+
+                // Animate Controls
+                if (newAngle === 0) {
+                    noFade.animate({ top: -68 }, function () {
+                        text.fadeIn();
+                    });
+                } else {
+                    text.fadeOut( function () {
+                        noFade.animate({ top: 298 });
+                    });
+                }
+            });
+
+            // Set up demo
+            var demo, button = this.$('#demo-btn');
+            button.click( function (e) {
+                if (!demo) demo = new Demo(canvas);
+                if (!demo.running) {
+                    demo.start();
+                    button.find('div.play').fadeOut();
+                    button.find('div.pause').fadeIn();
+                } else {
+                    demo.pause();
+                    button.find('div.pause').fadeOut();
+                    button.find('div.play').fadeIn();
+                }
+            });
         }
     });
 });
